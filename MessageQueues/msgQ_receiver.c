fdede710cb84860db78360b887a89556efe0a5dc
Asif Ahamed <asifahamed.ece.dev@gmail.com>
2026-07-19 20:33:01 +0530

    Added MessageQueue Receiver

---
+++ MessageQueues/msgQ_receiver.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <sys/msg.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define msg_key 0x1234
+
+//Msg Struct
+struct msgbuf {
+    long mtype; // always long and > 0.
+    char mtext[100];
+};
+
+int main()
+{
+    int msqid;
+    struct msgbuf msg;
+
+    // 1. Access the Already Created MsgQueue
+    msqid = msgget(msg_key, 0666);
+    if(msqid == -1){
+        perror("msgget failed!\n");
+        return 1;
+    }
+    printf("[Receiver]: Attached to MsgQueue ID: %d", msqid);
+
+    // 2. Lets Receive the HIGH Priority Msg first
+    if( msgrcv(msqid, &msg, sizeof(msg.mtext), 4, 0) == -1){
+        perror("msgrcv failed!\n");
+        return 2;
+    }
+    printf("[Receiver] Msg Type %ld: '%s'", msg.mtype, msg.mtext);
+
+    // 3. Receive the next available message irrespectivbe of Type
+    if(msgrcv(msqid, &msg, sizeof(msg.mtext), 0, 0) == -1){    // (Type 0 means "give me whatever is next")
+        perror("msgrcv failed!\n");
+        return 3;
+    }
+    printf("[Receiver] Msg Type %ld: '%s'", msg.mtype, msg.mtext);
+
+
+    // 4. Task Completed. Deleting Msg Queue
+    printf("[Receiver]: Deleting Message Queue!\n");
+    if ( msgctl(msqid, IPC_RMID, NULL) == -1){
+        perror("msgctl failed!\n");
+        return 4;
+    }
+
+    printf("[Receiver]: Cleanup Complete. MsgQueue Deleted.\n");
+    return 0;
+}
